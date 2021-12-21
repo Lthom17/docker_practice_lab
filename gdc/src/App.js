212d15664daf985ad5b834ba805f633036cb54e0
lthom17 <laurenthom17@gmail.com>
2021-12-21 12:25:29 -0600

    added image to homepage and added homepage to App.js

--- gdc/src/App.js
+++ gdc/src/App.js
@@ -4,7 +4,10 @@ import Homepage from './components/Homepage';
 class App extends React.Component {
   render() {
     return (
+      <>
       <h1>Great Diamond Club</h1>
+      <Homepage />
+      </>
     );
   }
 }
--- gdc/src/components/Homepage.js
+++ gdc/src/components/Homepage.js
@@ -1,34 +1,38 @@
 
+import image from "../images/homepage.jpg"
 
-function Homepage()
-{
-return(
-<div>
-    <div>NAVBAR: bootstrap </div>
-   <h1>{welcomeHomepage}</h1>
-   <div></div>
-   <img src= "./gdc/homepage.jpg"/>
-</div>
-)
+function Homepage() {
+    return (
+        <div>
+            <div>NAVBAR: bootstrap </div>
+            <h1>{welcomeHomepage}</h1>
+            <div>
+                <img className="img-fluid" src={image} />
+            </div>
+
+        </div>
+    )
 }
 
-function welcomeHomepage(){
-return "Welcome to the Great Diamond Club"; 
+export default Homepage;
+
+function welcomeHomepage() {
+    return "Welcome to the Great Diamond Club";
 }
 
-function fakeEmail(){
-return "contact@gdc.com";
+function fakeEmail() {
+    return "contact@gdc.com";
 
 }
 
-function fakePhoneNumber()
-{
+function fakePhoneNumber() {
     return "(919)252-8888";
 
 }
 
-function fakeAddress()
-{
+function fakeAddress() {
     return "1234 Wallabe Way Sydney, CA";
 }
+
+
 
