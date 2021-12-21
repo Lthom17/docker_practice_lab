b31478fcb8daca2d0b230f02c538664e35f767a8
jeantyg13 <jeantyg13@students.ecu.edu>
2021-12-21 14:17:39 -0500

    homepage adjustments

--- gdc/src/App.js
+++ gdc/src/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <h1>Great Diamond Club</h1>
       <Homepage />
       </>
     );
--- gdc/src/components/Homepage.js
+++ gdc/src/components/Homepage.js
@@ -5,7 +5,6 @@ function Homepage() {
     return (
 
         <body>
-        /* -- navbar -- */
             <div>
                 <nav class="navbar navbar-default">
                     <div class="container-fluid">
@@ -14,34 +13,24 @@ function Homepage() {
                         </div>
                         <ul class="nav navbar-nav">
                             <li class="active"><a href="#">Home</a></li>
-                            <li><a href="./products">Products</a></li> /* -- maybe cut -- */
                             <li><a href="./about">About Us</a></li>
                         </ul>
                     </div>
                 </nav>
             </div>
 
-        /* -- header -- */
             <header class="" />
             <div class="p-5 text-center bg-light">
-                <h1 class="mb-3">Homepage</h1>
-                <h4 class="mb-3">Great Diamond Club</h4>
+                <h1 class="mb-3">Great Diamond Club</h1>
                 <div>
                     <p>Beauty is about being comfortable in your own skin. That and some Great Diamond Club products.</p>
                 </div>
-                <a class="btn btn-primary" href="./products" role="button">Products</a>
-            </div>
-
-        /* -- feature product-- */
-            <section class="">
-                <div>
-                    <h4> Continue to products page
-                        <img className="img-fluid" src={image} />
-                    </h4>
+                <div className = "ins">
+                <img className="img-fluid" src={image} />
                 </div>
-            </section>
-
-            /* -- end -- */
+                <br/>
+                <a class="btn btn-primary" href="./products" role="button">Products </a>
+            </div>
         </body>
     )
 }
