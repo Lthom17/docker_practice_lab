4964c8f2a6d8e5250cdf84407853d6f7e52cadf6
jeantyg13 <jeantyg13@students.ecu.edu>
2021-12-21 13:52:33 -0500

    added homepage structure with navbar, header, products link

--- gdc/src/App.js
+++ gdc/src/App.js
@@ -4,7 +4,10 @@ import Homepage from './components/Homepage';
 class App extends React.Component {
   render() {
     return (
-      <h1>Great Diamond Club</h1>
+      <div>
+        <h1>Great Diamond Club</h1>
+        <h2>You've reached our old page. Click here to continue to new page!</h2>
+      </div>
     );
   }
 }
--- gdc/src/components/Homepage.js
+++ gdc/src/components/Homepage.js
@@ -1,34 +1,66 @@
 
 
-function Homepage()
-{
-return(
-<div>
-    <div>NAVBAR: bootstrap </div>
-   <h1>{welcomeHomepage}</h1>
-   <div></div>
-   <img src= "./gdc/homepage.jpg"/>
-</div>
-)
+function Homepage() {
+    return (
+
+        <body>
+        /* -- navbar -- */
+            <div>
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">GDC Homepage</a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="./products">Products</a></li> /* -- maybe cut -- */
+                            <li><a href="./about">About Us</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+        /* -- header -- */
+            <header class="" />
+            <div class="p-5 text-center bg-light">
+                <h1 class="mb-3">Homepage</h1>
+                <h4 class="mb-3">Great Diamond Club</h4>
+                <div>
+                    <p>Beauty is about being comfortable in your own skin. That and some Great Diamond Club products.</p>
+                </div>
+                <a class="btn btn-primary" href="./products" role="button">Products</a>
+            </div>
+
+        /* -- feature product-- */
+            <section class="">
+                <div>
+                    <h4> Continue to products page
+                        <img src="./gdc/homepage.jpg" />
+                    </h4>
+                </div>
+            </section>
+
+            /* -- end -- */
+        </body>
+    )
 }
 
-function welcomeHomepage(){
-return "Welcome to the Great Diamond Club"; 
+function welcomeHomepage() {
+    return "Welcome to the Great Diamond Club";
 }
 
-function fakeEmail(){
-return "contact@gdc.com";
+function fakeEmail() {
+    return "contact@gdc.com";
 
 }
 
-function fakePhoneNumber()
-{
+function fakePhoneNumber() {
     return "(919)252-8888";
 
 }
 
-function fakeAddress()
-{
+function fakeAddress() {
     return "1234 Wallabe Way Sydney, CA";
 }
 
+export default Homepage;
