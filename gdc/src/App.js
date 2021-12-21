d5b70b10f2609781e44854ae21918d6943905beb
rwu888-dev <rwu888@yahoo.com>
2021-12-21 11:12:39 -0800

    AboutUs finished

--- gdc/src/App.js
+++ gdc/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import AboutUs from './components/AboutUs';
+import NavBar from  './components/NavBar';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Great Diamond Club</h1>
+      <Router>
+
+        <NavBar/>
+
+        <Route exact path ='/'>
+          <Homepage/>
+        </Route>
+
+        <Route exact path ='/products'>
+          <Products/>
+        </Route>
+
+        <Route exact path='/about-us'>
+          <AboutUs/>
+        </Route>
+
+      </Router>
+
     );
   }
 }
---
+++ gdc/src/components/NavBar.js
@@ -0,0 +1,25 @@
+function NavBar() {
+    return( 
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand" href="#">Great Diamond Club</a>
+            <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-navigation">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
